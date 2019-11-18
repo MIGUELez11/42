1b9718753bcfe26d64a11ca3dcdea4c9c99fbd84
Miguel angel Florido cifuentes <mflorido@c3r8s3.42madrid.com>
2019-11-18 14:50:57 +0100

    Libft

---
+++ Cursus/libft/part1Functions/ft_strlcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mflorido <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/09 09:22:01 by mflorido          #+#    #+#             */
+/*   Updated: 2019/11/13 10:42:56 by mflorido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t n)
+{
+	size_t i;
+	size_t dstsize;
+	size_t srcsize;
+	size_t r;
+
+	dstsize = ft_strlen(dst);
+	srcsize = ft_strlen(src);
+	if (n < dstsize)
+		r = (srcsize + n);
+	else if (n == dstsize)
+		r = (dstsize + n - 1);
+	else
+		r = (dstsize + srcsize);
+	i = dstsize;
+	while (src[i - dstsize] && i < n - 1)
+	{
+		dst[i] = src[i - dstsize];
+		i++;
+	}
+	if (n == 0)
+		i = dstsize;
+	dst[i] = 0;
+	return (r);
+}
---
+++ Cursus/libft/part2Functions/ft_strtrim.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mflorido <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 11:11:00 by mflorido          #+#    #+#             */
+/*   Updated: 2019/11/13 18:33:03 by mflorido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+static int	ft_containschar(char const *s1, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] != c)
+		i++;
+	if (s1[i] == c)
+		return (1);
+	return (0);
+}
+
+static char	*ft_allocmem(char const *s1, size_t j, size_t k)
+{
+	size_t size;
+
+	size = ft_strlen(s1);
+	if (k == -1)
+		return (ft_calloc(1, sizeof(char)));
+	else
+		return (ft_calloc(size - j - (size - k) + 2, sizeof(char)));
+}
+
+char		*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	*r;
+
+	if (s1 == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	k = ft_strlen(s1) - 1;
+	while (s1[j] && ft_containschar(set, s1[j]))
+		j++;
+	while (s1[k] && ft_containschar(set, s1[k]))
+		k--;
+	r = ft_allocmem(s1, j, k);
+	if (r == NULL)
+		return (NULL);
+	while (i + j <= k && s1[i + j])
+	{
+		r[i] = s1[i + j];
+		i++;
+	}
+	r[i] = 0;
+	return (r);
+}
