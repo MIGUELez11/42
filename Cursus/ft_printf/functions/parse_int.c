b26ae65c536ca966fe246be2367a9e60d540c356
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2020-01-30 18:43:31 +0100

    Trying to make integers work

--- Cursus/ft_printf/functions/parse_int.c
+++ Cursus/ft_printf/functions/parse_int.c
@@ -6,13 +6,70 @@
 /*   By: mflorido <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 13:56:09 by mflorido          #+#    #+#             */
-/*   Updated: 2020/01/30 17:12:36 by mflorido         ###   ########.fr       */
+/*   Updated: 2020/01/30 18:42:33 by mflorido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 int		parse_int(t_printf_list *p_lst)
+{
+	long int	n;
+	int			len;
+
+	n = va_arg(p_lst->ap, int);
+	len = ft_intlen(n);
+//
+	if (p_lst->flags.precision - len > 0)
+		len = p_lst->flags.precision;
+//
+//
+	if (p_lst->flags.width - len > 0 && !p_lst->flags.zero && !p_lst->flags.minus)
+	{
+		put_repeated_char(' ', p_lst->flags.width - len);
+		p_lst->printed_chars += p_lst->flags.width - len;
+	}
+//
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		p_lst->printed_chars += 1;
+	}
+//
+	if (p_lst->flags.width - len > 0 && p_lst->flags.zero && !p_lst->flags.minus)
+	{
+		put_repeated_char('0', p_lst->flags.width - len);
+		p_lst->printed_chars += p_lst->flags.width - len;
+	}
+//
+//
+	if (p_lst->flags.precision > 0)
+	{
+		put_repeated_char('0', p_lst->flags.precision - ( n < 0 ? len + 1 : len));
+		p_lst->printed_chars += p_lst->flags.precision - ( n < 0 ? len + 1 : len);
+	}
+//
+	putnbr_abs(n, 1);
+	p_lst->printed_chars += len;
+//
+	if (p_lst->flags.width < 0 || p_lst->flags.minus)
+	{
+		if (p_lst->flags.width < 0)
+		{
+			p_lst->flags.minus = 1;
+			p_lst->flags.width *= -1;
+		}
+		if (p_lst->flags.width - len > 0)
+		{
+			put_repeated_char(' ', p_lst->flags.width - len);
+			p_lst->printed_chars += p_lst->flags.width - len;
+		}
+	}
+//
+	return (1);
+}
+/*int		parse_int(t_printf_list *p_lst)
 {
 	long int	n;
 	int			len;
@@ -37,4 +94,4 @@ int		parse_int(t_printf_list *p_lst)
 	p_lst->printed_chars += len;
 	neg_width(p_lst, (n < 0 ? len + 1 : len));
 	return (1);
-}
+}*/
