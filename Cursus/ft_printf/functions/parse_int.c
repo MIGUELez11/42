87ea1a15df13509cd8c8bfbbbfe9a37329327546
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2020-02-03 19:55:08 +0100

    Adding width to int

--- Cursus/ft_printf/functions/parse_int.c
+++ Cursus/ft_printf/functions/parse_int.c
@@ -6,7 +6,7 @@
 /*   By: mflorido <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 13:56:09 by mflorido          #+#    #+#             */
-/*   Updated: 2020/01/30 18:42:33 by mflorido         ###   ########.fr       */
+/*   Updated: 2020/02/03 19:52:46 by mflorido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,54 +19,37 @@ int		parse_int(t_printf_list *p_lst)
 
 	n = va_arg(p_lst->ap, int);
 	len = ft_intlen(n);
-//
-	if (p_lst->flags.precision - len > 0)
-		len = p_lst->flags.precision;
-//
-//
-	if (p_lst->flags.width - len > 0 && !p_lst->flags.zero && !p_lst->flags.minus)
+	if (p_lst->flags.width < 0)
 	{
-		put_repeated_char(' ', p_lst->flags.width - len);
-		p_lst->printed_chars += p_lst->flags.width - len;
+		p_lst->flags.width *= -1;
+		p_lst->flags.minus = 1;
 	}
-//
+	if (n < 0 && p_lst->flags.width > 0)
+		p_lst->flags.width--;
 
+	if (p_lst->flags.width && !p_lst->flags.minus &&  p_lst->flags.width > len && p_lst->flags.width > p_lst->flags.precision)
+	{
+		put_repeated_char(' ', p_lst->flags.width - (p_lst->flags.precision > len ? p_lst->flags.precision : len));
+		p_lst->printed_chars += p_lst->flags.width - (p_lst->flags.precision > len ? p_lst->flags.precision : len);
+	}
 	if (n < 0)
 	{
 		write(1, "-", 1);
 		p_lst->printed_chars += 1;
 	}
-//
-	if (p_lst->flags.width - len > 0 && p_lst->flags.zero && !p_lst->flags.minus)
+	if((!p_lst->flags.minus && p_lst->flags.width > len && p_lst->flags.width > p_lst->flags.precision && p_lst->flags.zero) || (p_lst->flags.precision > len))
 	{
-		put_repeated_char('0', p_lst->flags.width - len);
-		p_lst->printed_chars += p_lst->flags.width - len;
+		put_repeated_char('0', (p_lst->flags.width > p_lst->flags.precision ? p_lst->flags.width : p_lst->flags.precision) - len);
+		p_lst->printed_chars += (p_lst->flags.width > p_lst->flags.precision ? p_lst->flags.width : p_lst->flags.precision) - len; 
 	}
-//
-//
-	if (p_lst->flags.precision > 0)
-	{
-		put_repeated_char('0', p_lst->flags.precision - ( n < 0 ? len + 1 : len));
-		p_lst->printed_chars += p_lst->flags.precision - ( n < 0 ? len + 1 : len);
-	}
-//
 	putnbr_abs(n, 1);
 	p_lst->printed_chars += len;
-//
-	if (p_lst->flags.width < 0 || p_lst->flags.minus)
+	if (p_lst->flags.minus && p_lst->flags.width > len && p_lst->flags.width > p_lst->flags.precision)
 	{
-		if (p_lst->flags.width < 0)
-		{
-			p_lst->flags.minus = 1;
-			p_lst->flags.width *= -1;
-		}
-		if (p_lst->flags.width - len > 0)
-		{
-			put_repeated_char(' ', p_lst->flags.width - len);
-			p_lst->printed_chars += p_lst->flags.width - len;
-		}
+		put_repeated_char(' ', p_lst->flags.width - (p_lst->flags.precision > len ? p_lst->flags.precision : len));
+		p_lst->printed_chars += p_lst->flags.width - (p_lst->flags.precision > len ? p_lst->flags.precision : len); 
 	}
-//
+	
 	return (1);
 }
 /*int		parse_int(t_printf_list *p_lst)
